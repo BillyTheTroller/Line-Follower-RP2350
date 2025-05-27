231d733a5e6b77b7bc014d41b6ac3d075ac8812e
Καραμπιμπέρης Γεώργιος <georgekarabiberis5@gmail.com>
2025-05-27 20:21:02 +0300

    Update wifi.py

--- src/wifi.py
+++ src/wifi.py
@@ -1,11 +1,13 @@
 #wifi.py
-
+#Δημιουργία του web server για την διασύνδεση του ρομπότ με το app
 import network
 import time
 
+# Ρύθμιση του SSID και password του mobile network που ενεργοποιέιται από το mobile hotspot
 SSID = 'Jeff'
 PASSWORD = 'iuvv1308'
 
+# Σύνδεση του Pico2W με το web app
 def connect():
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
